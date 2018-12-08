56dc7a59eb95cbca498c28bed2ff09eddc1a5b6d
jethrogillgren <jethrogillgren@gmail.com>
2018-12-08 16:40:08 +0000

    Sounds and Commands

--- Centerpiece/Centerpiece.ino
+++ Centerpiece/Centerpiece.ino
@@ -1,3 +1,10 @@
+/*
+ * --------------------------------------------------------------------------------------------------------------------
+ * XBee comms control 5 RGB LEDs and a keypad/buzzer.
+ * --------------------------------------------------------------------------------------------------------------------
+ *
+ */
+
 #include <Printers.h>
 #include <Keypad.h>
 #include <Adafruit_NeoPixel.h>
@@ -64,20 +71,20 @@ int fadeDelay = 20; //speed of fade
 
 /*//Strip LEDs - These are WS2811 LEDs powered by 12V - used for the Activation lines between KoalaPos
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
-int creatorNumLEDs = 10;
-Adafruit_NeoPixel lhsCreatorPixels = Adafruit_NeoPixel(creatorNumLEDs, 4,
-NEO_GRB + NEO_KHZ800);//Ring around the LHS Creator KoalaPos
-Adafruit_NeoPixel rhsCreatorPixels = Adafruit_NeoPixel(creatorNumLEDs, 7,
-NEO_GRB + NEO_KHZ800);//Ring around the RHS Creator KoalaPos
+  // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+  // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+  // example for more information on possible values.
+  int creatorNumLEDs = 10;
+  Adafruit_NeoPixel lhsCreatorPixels = Adafruit_NeoPixel(creatorNumLEDs, 4,
+  NEO_GRB + NEO_KHZ800);//Ring around the LHS Creator KoalaPos
+  Adafruit_NeoPixel rhsCreatorPixels = Adafruit_NeoPixel(creatorNumLEDs, 7,
+  NEO_GRB + NEO_KHZ800);//Ring around the RHS Creator KoalaPos
 
-int creatorToCombinorNumLEDs = 10;
-Adafruit_NeoPixel lhsCreatorToCombinorPixels
-= Adafruit_NeoPixel(creatorToCombinorNumLEDs, 8, NEO_GRB + NEO_KHZ800);//Creator->Combinor lhs
-Adafruit_NeoPixel rhsCreatorToCombinorPixels
-= Adafruit_NeoPixel(creatorToCombinorNumLEDs, 12, NEO_GRB + NEO_KHZ800);//Creator->Combinor rhs
+  int creatorToCombinorNumLEDs = 10;
+  Adafruit_NeoPixel lhsCreatorToCombinorPixels
+  = Adafruit_NeoPixel(creatorToCombinorNumLEDs, 8, NEO_GRB + NEO_KHZ800);//Creator->Combinor lhs
+  Adafruit_NeoPixel rhsCreatorToCombinorPixels
+  = Adafruit_NeoPixel(creatorToCombinorNumLEDs, 12, NEO_GRB + NEO_KHZ800);//Creator->Combinor rhs
 */
 
 //// XBEE & COMMUNICATIONS
@@ -92,7 +99,18 @@ XBeeWithCallbacks xbee;
 #define MSG_WATER_PRESSED 'W'
 #define MSG_EARTH_PRESSED 'e'
 
-#define MSG_RESET   'r'
+#define MSG_WOOD_CREATOR 'g'
+#define MSG_METAL_CREATOR 'w'
+#define MSG_FIRE_CREATOR 'r'
+#define MSG_WATER_CREATOR 'b'
+#define MSG_EARTH_CREATOR 'y'
+#define MSG_WOOD_COMBINOR 'G'
+#define MSG_METAL_COMBINOR 'W'
+#define MSG_FIRE_COMBINOR 'R'
+#define MSG_WATER_COMBINOR 'B'
+#define MSG_EARTH_COMBINOR 'Y'
+
+#define MSG_RESET   'n'
 
 // Build a reuseable message packet to send to the Co-Ordinator
 XBeeAddress64 coordinatorAddr = XBeeAddress64(0x00000000, 0x00000000);
@@ -104,7 +122,7 @@ ZBTxRequest pressMessage = ZBTxRequest(coordinatorAddr, pressMessagePayload, siz
 
 
 //// SOUND
- 
+
 //#include <Tone.h> <-- This collided with using PWM Timer on Pins 3 and 11
 //Tone notePlayer;
 
@@ -128,11 +146,11 @@ int notes[] = { 440,
 
 void setup() {
   Serial.begin(9600);
-  
+
   //notePlayer.begin(buzzerPin);
 
   //pinMode(woodPress, INPUT); <-- Analogue pins do not use pinMode to initialize
-  //digitalWrite(woodPress, HIGH); 
+  //digitalWrite(woodPress, HIGH);
   //pinMode(metalPress, INPUT);
   //digitalWrite(metalPress, HIGH);
   pinMode(firePress, INPUT);// initialize the pushbutton pin as an input:
@@ -141,7 +159,7 @@ void setup() {
   digitalWrite(waterPress, HIGH);
   pinMode(earthPress, INPUT);
   digitalWrite(earthPress, HIGH);
-  
+
   pinMode(woodLED, OUTPUT);
   pinMode(metalLED, OUTPUT);
   pinMode(fireLED, OUTPUT);
@@ -149,10 +167,10 @@ void setup() {
   pinMode(earthLED, OUTPUT);
 
   /*//Initialize the NeoPixel library.
-  lhsCreatorPixels.begin();
-  rhsCreatorPixels.begin();
-  lhsCreatorToCombinorPixels.begin();
-  rhsCreatorToCombinorPixels.begin();*/
+    lhsCreatorPixels.begin();
+    rhsCreatorPixels.begin();
+    lhsCreatorToCombinorPixels.begin();
+    rhsCreatorToCombinorPixels.begin();*/
 
   // WHen resetting the room, we want to see that the lights are working.
   //They will go unlit when the Game starts and send a Rest Command.
@@ -161,7 +179,6 @@ void setup() {
   PlayGoodSound();
   SetAllLEDs(0);
 
-  
   // XBEE
   xbeeSerial.begin(9600);
   xbee.setSerial(xbeeSerial);
@@ -185,15 +202,10 @@ void setup() {
 
 
   Serial.println("SETUP OK");
-  
+
 }
 
 void loop() {
-
-
-  //// SOUND
-  //notePlayer.stop();
-
 
   //// NETWORK COMMANDS
   // Continuously let xbee read packets and call callbacks.
@@ -228,33 +240,33 @@ void loop() {
   }
 
   //// LEDs
-  if( woodBrightness < woodTarget )
+  if ( woodBrightness < woodTarget )
   {
-    woodBrightness+=fadeAmount;
+    woodBrightness += fadeAmount;
     analogWrite(woodLED, woodBrightness);
     delay( fadeDelay);
   }
-  if( metalBrightness < metalTarget )
+  if ( metalBrightness < metalTarget )
   {
-    metalBrightness+=fadeAmount;
+    metalBrightness += fadeAmount;
     analogWrite(metalLED, metalBrightness);
     delay( fadeDelay);
   }
-  if( fireBrightness < fireTarget )
+  if ( fireBrightness < fireTarget )
   {
-    fireBrightness+=fadeAmount;
+    fireBrightness += fadeAmount;
     analogWrite(fireLED, fireBrightness);
     delay( fadeDelay);
   }
-  if( waterBrightness < waterTarget)
+  if ( waterBrightness < waterTarget)
   {
-    waterBrightness+=fadeAmount;
+    waterBrightness += fadeAmount;
     analogWrite(waterLED, waterBrightness);
     delay( fadeDelay);
   }
-  if( earthBrightness < earthTarget )
+  if ( earthBrightness < earthTarget )
   {
-    earthBrightness+=fadeAmount;
+    earthBrightness += fadeAmount;
     analogWrite(earthLED, earthBrightness);
     delay( fadeDelay);
   }
@@ -266,31 +278,24 @@ void loop() {
 
     Serial.println(key + " pressed");
     SendPressedMessage( key ); //This goes to the Game Server
-    
+
     //SOUND
     switch (key)
     {
       case '1':
-        //notePlayer.play(notes[0]);
-        //delay(keyDelay);
         TimerFreeTone(TONE_PIN, notes[1], keyDelay);
-        woodBrightness=1;
         break;
       case '2':
         TimerFreeTone(TONE_PIN, notes[2], keyDelay);
-        metalBrightness=1;
         break;
       case '3':
         TimerFreeTone(TONE_PIN, notes[3], keyDelay);
-        fireBrightness=1;
         break;
       case '4':
         TimerFreeTone(TONE_PIN, notes[4], keyDelay);
-        waterBrightness =1;
         break;
       case '5':
         TimerFreeTone(TONE_PIN, notes[5], keyDelay);
-        earthBrightness=1;
         break;
       case '6':
         TimerFreeTone(TONE_PIN, notes[6], keyDelay);
@@ -338,57 +343,66 @@ void zbReceive(ZBRxResponse& rx, uintptr_t data) {
   if (rx.getOption() == ZB_PACKET_ACKNOWLEDGED
       || rx.getOption() == ZB_BROADCAST_PACKET ) {
 
-      //Debug it out - copied from the lib
-      Print *p = (Print*)data;
-      if (!p) {
-        Serial.println("ERROR 2");
-        //flashSingleLed(LED_BUILTIN, 2, 500);
-        return;
-      }
-      p->println(F("Recieved:"));
-        p->print("  Payload: ");
-        printHex(*p, rx.getFrameData() + rx.getDataOffset(), rx.getDataLength(), F(" "), F("\r\n    "), 8);
-      p->println();
-        p->print("  From: ");
-        printHex(*p, rx.getRemoteAddress64() );
-      p->println();
+    //Debug it out - copied from the lib
+    Print *p = (Print*)data;
+    if (!p) {
+      Serial.println("ERROR 2");
+      //flashSingleLed(LED_BUILTIN, 2, 500);
+      return;
+    }
+    p->println(F("Recieved:"));
+    p->print("  Payload: ");
+    printHex(*p, rx.getFrameData() + rx.getDataOffset(), rx.getDataLength(), F(" "), F("\r\n    "), 8);
+    p->println();
+    p->print("  From: ");
+    printHex(*p, rx.getRemoteAddress64() );
+    p->println();
 
-      //This Project only ever takes a 1 char command
-      //printHex(rx.getData()[0], 2);
-      parseCommand( (char) rx.getData()[0] );
-      
-      //flashSingleLed(LED_BUILTIN, 5, 50);
-      
+    //This Project only ever takes a 1 char command
+    //printHex(rx.getData()[0], 2);
+    parseCommand( (char) rx.getData()[0] );
+
+    //flashSingleLed(LED_BUILTIN, 5, 50);
+
   } else {
-      // we got it (obviously) but sender didn't get an ACK
-      Serial.println("ERROR 1");
-      //flashSingleLed(LED_BUILTIN, 1, 500);
+    // we got it (obviously) but sender didn't get an ACK
+    Serial.println("ERROR 1");
+    //flashSingleLed(LED_BUILTIN, 1, 500);
   }
 }
 
 
 
 
-int badMelody[] =      {262, 196, 196, 220, 196, 0,   247, 262};
-int badMelodyDelay[] = {250, 125, 125, 250, 250, 250, 250, 250};
+int badMelody[] =      {196, 196, 196};
+int badMelodyDelay[] = {125, 125, 125};
+int goodMelody[] =      {440, 659, 783, 880, 1046};
+int goodMelodyDelay[] = {125, 125,  42,  42,   42};
+int tuneMelody[] =      {262, 196, 196, 220, 196, 0,   247, 262};
+int tuneMelodyDelay[] = {250, 125, 125, 250, 250, 250, 250, 250};
 
 //Blocking
 void PlayBadSound()
 {
-  for( int thisNote = 0; thisNote < 8; thisNote++)
+  for ( int thisNote = 0; thisNote < 3; thisNote++)
   {
     TimerFreeTone(TONE_PIN, badMelody[thisNote], badMelodyDelay[thisNote]);
   }
 }
-int goodMelody[] =      {262, 196, 196, 220, 196, 0,   247, 262};
-int goodMelodyDelay[] = {250, 125, 125, 250, 250, 250, 250, 250};
-
 //Blocking
 void PlayGoodSound()
 {
-  for( int thisNote = 0; thisNote < 8; thisNote++)
+  for ( int thisNote = 0; thisNote < 5; thisNote++)
   {
     TimerFreeTone(TONE_PIN, goodMelody[thisNote], goodMelodyDelay[thisNote]);
+  }
+}
+//Blocking
+void PlayTune()
+{
+  for ( int thisNote = 0; thisNote < 8; thisNote++)
+  {
+    TimerFreeTone(TONE_PIN, tuneMelody[thisNote], tuneMelodyDelay[thisNote]);
   }
 }
 
@@ -396,78 +410,82 @@ void PlayGoodSound()
 
 void ResetAllLEDs()
 {
-    SetAllLEDs(0);
+  SetAllLEDs(0);
 
-    /*SetNeopixelColourNone(lhsCreatorPixels, creatorNumLEDs);
+  /*SetNeopixelColourNone(lhsCreatorPixels, creatorNumLEDs);
     SetNeopixelColourNone(rhsCreatorPixels, creatorNumLEDs);
     SetNeopixelColourNone(lhsCreatorToCombinorPixels, creatorToCombinorNumLEDs);
     SetNeopixelColourNone(rhsCreatorToCombinorPixels, creatorToCombinorNumLEDs);
-    */
+  */
 }
 
 //Set LEDs to white at the amount specified (0-255)
 void SetAllLEDs( int amount)
 {
-    analogWrite(woodLED, amount);
-    analogWrite(fireLED, amount);
-    analogWrite(waterLED, amount);
-    analogWrite(metalLED, amount);
-    analogWrite(earthLED, amount);
-    Serial.print("Set all LEDs: ");
-    Serial.println(amount);
+  analogWrite(woodLED, amount);
+  analogWrite(fireLED, amount);
+  analogWrite(waterLED, amount);
+  analogWrite(metalLED, amount);
+  analogWrite(earthLED, amount);
+  Serial.print("Set all LEDs: ");
+  Serial.println(amount);
 }
 
- void FadeAllLEDs(int amount)
- {
-    woodTarget = amount;
-    fireTarget = amount;
-    metalTarget = amount;
-    waterTarget = amount;
-    earthTarget = amount;
+void FadeAllLEDs(int amount)
+{
+  woodTarget = amount;
+  fireTarget = amount;
+  metalTarget = amount;
+  waterTarget = amount;
+  earthTarget = amount;
 
-    /*SetNeopixelColour(lhsCreatorPixels, creatorNumLEDs, amount, amount, amount);
+  /*SetNeopixelColour(lhsCreatorPixels, creatorNumLEDs, amount, amount, amount);
     SetNeopixelColour(rhsCreatorPixels, creatorNumLEDs, amount, amount, amount);
     SetNeopixelColour(lhsCreatorToCombinorPixels, creatorToCombinorNumLEDs, amount, amount, amount);
     SetNeopixelColour(rhsCreatorToCombinorPixels, creatorToCombinorNumLEDs, amount, amount, amount);
-    */
+  */
 }
 
 
 
-/*////Neopixel LED FUNCTIONS 
-void SetNeopixelColourRed( Adafruit_NeoPixel neopixel, int numLEDs ) {
+/*////Neopixel LED FUNCTIONS
+  void SetNeopixelColourRed( Adafruit_NeoPixel neopixel, int numLEDs ) {
   SetNeopixelColour(neopixel, numLEDs, 255,0,0);
-}
-void SetNeopixelColourGreen( Adafruit_NeoPixel neopixel, int numLEDs ) {
+  }
+  void SetNeopixelColourGreen( Adafruit_NeoPixel neopixel, int numLEDs ) {
   SetNeopixelColour(neopixel, numLEDs, 0,255,0);
-}
-void SetNeopixelColourYellow( Adafruit_NeoPixel neopixel, int numLEDs ) {
+  }
+  void SetNeopixelColourYellow( Adafruit_NeoPixel neopixel, int numLEDs ) {
   SetNeopixelColour(neopixel, numLEDs, 255,255,0);
-}
-void SetNeopixelColourWhite( Adafruit_NeoPixel neopixel, int numLEDs ) {
+  }
+  void SetNeopixelColourWhite( Adafruit_NeoPixel neopixel, int numLEDs ) {
   SetNeopixelColour(neopixel, numLEDs, 255,255,255);
-}
-void SetNeopixelColourBlue( Adafruit_NeoPixel neopixel, int numLEDs ) {
+  }
+  void SetNeopixelColourBlue( Adafruit_NeoPixel neopixel, int numLEDs ) {
   SetNeopixelColour(neopixel, numLEDs, 0,0,255);
-}
-void SetNeopixelColourNone( Adafruit_NeoPixel neopixel, int numLEDs ) {
+  }
+  void SetNeopixelColourNone( Adafruit_NeoPixel neopixel, int numLEDs ) {
   SetNeopixelColour(neopixel, numLEDs, 0,0,0);
-}
-void SetNeopixelColour(Adafruit_NeoPixel neopixel, int numLEDs, int red, int green, int blue)
-{
+  }
+  void SetNeopixelColour(Adafruit_NeoPixel neopixel, int numLEDs, int red, int green, int blue)
+  {
   /*for(int i=0;i<numLEDs;i++){
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
     neopixel.setPixelColor(i, neopixel.Color(red, green, blue));
     neopixel.show(); // This sends the updated pixel color to the hardware.
     delay(10);
   }
-}*/
+  }*/
 
 
 
 // Parse serial input, take action if it's a valid character
 void parseCommand(char c)
 {
+  Serial.print(F("Handling command: 0x" ));
+  Serial.print(c, HEX);
+  Serial.println();
+  
   switch (c)
   {
     case 's': // Successful Code
@@ -478,83 +496,82 @@ void parseCommand(char c)
       PlayBadSound();
       break;
 
-    case 'g': // Light the Wood Creator LEDs
+    case MSG_WOOD_CREATOR: // Light the Wood Creator LEDs
       woodTarget = halfLitBrightness; //Start lighting the Button - the main loop will fade it up
       /*SetNeopixelColourGreen( lhsCreatorPixels, creatorNumLEDs );
-      SetNeopixelColourGreen( rhsCreatorPixels, creatorNumLEDs );*/
+        SetNeopixelColourGreen( rhsCreatorPixels, creatorNumLEDs );*/
 
       PlayGoodSound();
       break;
-    case 'G': // Light the Combinor Green LEDs
-      woodTarget = 255; 
+    case MSG_WOOD_COMBINOR: // Light the Combinor Green LEDs
+      woodTarget = 255;
       //SetNeopixelColourGreen( lhsCreatorToCombinorPixels, creatorNumLEDs );
       //SetNeopixelColourGreen( rhsCreatorToCombinorPixels, creatorNumLEDs );
-      
+
       PlayGoodSound();
       break;
 
-    case 'r': // Light the Fire Creator LEDs
+    case MSG_FIRE_CREATOR: // Light the Fire Creator LEDs
       fireTarget = halfLitBrightness; //Start lighting the Button - the main loop will fade it up
       //SetNeopixelColourRed( lhsCreatorPixels, creatorNumLEDs );
       //SetNeopixelColourRed( rhsCreatorPixels, creatorNumLEDs );
 
       PlayGoodSound();
       break;
-    case 'R': // Light the Combinor Red LEDs
+    case MSG_FIRE_COMBINOR: // Light the Combinor Red LEDs
       fireTarget = 255;
       //SetNeopixelColourRed( lhsCreatorToCombinorPixels, creatorNumLEDs );
       //SetNeopixelColourRed( rhsCreatorToCombinorPixels, creatorNumLEDs );
-      
+
       PlayGoodSound();
       break;
 
-    case 'b': // Light the Water Creator LEDs
+    case MSG_WATER_CREATOR: // Light the Water Creator LEDs
       waterTarget = halfLitBrightness; //Start lighting the Button - the main loop will fade it up
       //SetNeopixelColourBlue( lhsCreatorPixels, creatorNumLEDs );
       //SetNeopixelColourBlue( rhsCreatorPixels, creatorNumLEDs );
 
       PlayGoodSound();
       break;
-    case 'B': // Light the Combinor Blue LEDs
+    case MSG_WATER_COMBINOR: // Light the Combinor Blue LEDs
       waterTarget = 255;
       //SetNeopixelColourBlue( lhsCreatorToCombinorPixels, creatorNumLEDs );
       //SetNeopixelColourBlue( rhsCreatorToCombinorPixels, creatorNumLEDs );
-      
+
       PlayGoodSound();
       break;
 
-    case 'w': // Light the Metal Creator LEDs
+    case MSG_METAL_CREATOR: // Light the Metal Creator LEDs
       metalTarget = halfLitBrightness; //Start lighting the Button - the main loop will fade it up
       //SetNeopixelColourWhite( lhsCreatorPixels, creatorNumLEDs );
       //SetNeopixelColourWhite( rhsCreatorPixels, creatorNumLEDs );
 
       PlayGoodSound();
       break;
-    case 'W': // Light the Combinor White LEDs
-      metalTarget = 255; 
+    case MSG_METAL_COMBINOR: // Light the Combinor White LEDs
+      metalTarget = 255;
       //SetNeopixelColourWhite( lhsCreatorToCombinorPixels, creatorNumLEDs );
       //SetNeopixelColourWhite( rhsCreatorToCombinorPixels, creatorNumLEDs );
-      
+
       PlayGoodSound();
       break;
 
-    case 'y': // Light the Earth Creator LEDs
+    case MSG_EARTH_CREATOR: // Light the Earth Creator LEDs
       earthTarget = halfLitBrightness; //Start lighting the Button - the main loop will fade it up
       //SetNeopixelColourYellow( lhsCreatorPixels, creatorNumLEDs );
       //SetNeopixelColourYellow( rhsCreatorPixels, creatorNumLEDs );
 
       PlayGoodSound();
       break;
-    case 'Y': // Light the Combinor Yellow LEDs
+    case MSG_EARTH_COMBINOR: // Light the Combinor Yellow LEDs
       earthTarget = 255;
       //SetNeopixelColourYellow( lhsCreatorToCombinorPixels, creatorNumLEDs );
       //SetNeopixelColourYellow( rhsCreatorToCombinorPixels, creatorNumLEDs );
-      
+
       PlayGoodSound();
       break;
 
-    case 'n': // RESET - Turn off all RGB LEDs
-    case 'N': // RESET - Turn off all RGB LEDs
+    case MSG_RESET: // Turn off all RGB LEDs
       ResetAllLEDs();
       break;
 
@@ -565,34 +582,34 @@ void parseCommand(char c)
 
 void SendPressedMessage( char msg )
 {
-    pressMessagePayload[0] = msg;
-    pressMessage.setFrameId(xbee.getNextFrameId());
+  pressMessagePayload[0] = msg;
+  pressMessage.setFrameId(xbee.getNextFrameId());
 
-    Serial.print(F("SENDING Message to Co-ordinator: " ));
-    Serial.print(pressMessagePayload[0], HEX);
+  Serial.print(F("SENDING Message to Co-ordinator: " ));
+  Serial.print(pressMessagePayload[0], HEX);
+  Serial.println();
+
+  // Send the command and wait up to N ms for a response.  xbee loop continues during this time.
+  uint8_t status = xbee.sendAndWait(pressMessage, 5000);
+  if (status == 0)
+  {
+    Serial.println(F("SEND ACKNOWLEDGED"));
+  }
+  else
+  {
+    Serial.print(F("SEND FAILED: "));
+    printHex(status, 2);
     Serial.println();
-    
-    // Send the command and wait up to N ms for a response.  xbee loop continues during this time.
-    uint8_t status = xbee.sendAndWait(pressMessage, 5000);
-    if (status == 0)
-    {
-      Serial.println(F("SEND ACKNOWLEDGED"));
-    }
-    else
-    { 
-      Serial.print(F("SEND FAILED: "));
-      printHex(status, 2);
-      Serial.println();
-    }
+  }
 }
 
 // UTIL FUNCTIONS
 void printHex(int num, int precision) {
-     char tmp[16];
-     char format[128];
+  char tmp[16];
+  char format[128];
 
-     sprintf(format, "0x%%.%dX", precision);
+  sprintf(format, "0x%%.%dX", precision);
 
-     sprintf(tmp, format, num);
-     Serial.print(tmp);
+  sprintf(tmp, format, num);
+  Serial.print(tmp);
 }
